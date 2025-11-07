230edd1a556649e9831c3a929ada2cd4c5427cce
merciarobotics <merciarobotics@gmail.com>
2025-11-07 16:22:15 +0000

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSLocalizer.java
@@ -28,7 +28,7 @@ public class OTOSLocalizer implements Localizer {
     private Pose2d currentPose;
 
     public OTOSLocalizer(HardwareMap hardwareMap, Pose2d initialPose) {
-        // TODO: make sure your config has an OTOS device with this name
+        // TODO: make sure your config has an OTOS device with this name ay
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
         otos = hardwareMap.get(SparkFunOTOS.class, "sensor_otos");
         currentPose = initialPose;
